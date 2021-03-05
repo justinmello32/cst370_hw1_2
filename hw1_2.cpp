9cd1f8f76623f95cddaba1bdec9bd94dd2566631
Justin Mello <mello23@llnl.gov>
2021-03-05 11:51:05 -0800

    Initial Commit.

---
+++ hw1_2.cpp
@@ -0,0 +1,22 @@
+/*
+ * Title: hw1_2.cpp
+ * Abstract: This program reads reads two groups of numbers in which
+ *           each group has several integer numbers without duplicates.
+ *           The program should display the non-common numbers in both
+ *           groups in the descending order.
+ * Author: Justin Mello
+ * ID: 2002
+ * Date: 03/05/2021
+ */
+
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int main() {
+
+
+    return 0;
+}
+

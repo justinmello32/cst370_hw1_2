c0c8d404bff165318a6020c06e9be8b12d186847
Justin Mello <mello23@llnl.gov>
2021-03-05 14:51:11 -0800

    Comparing first array with second.

--- hw1_2.cpp
+++ hw1_2.cpp
@@ -12,9 +12,53 @@
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 int main() {
+
+    int groupOneAmount = 0;
+    int groupTwoAmount = 0;
+    bool matchFound = false;
+
+    cin >> groupOneAmount;
+    int groupOne[groupOneAmount];
+
+    //Setup array for first group
+    for(int i = 0; i < groupOneAmount; i++) {
+        cin >> groupOne[i];
+    }
+
+    cin >> groupTwoAmount;
+    int groupTwo[groupTwoAmount];
+
+    //Setup array for second Group
+    for(int j = 0; j < groupTwoAmount; j++) {
+        cin >> groupTwo[j];
+    }
+
+    //Sort both arrays
+    sort(groupOne, groupOne + groupOneAmount);
+    sort(groupTwo, groupTwo + groupTwoAmount);
+
+    vector<int> groupThree;
+
+
+    //Compare arrays
+    for(int i = 0; i < groupOneAmount; i++) {
+        for(int j = 0; j < groupTwoAmount; j++) {
+            if(groupOne[i] == groupTwo[j]) {
+                matchFound = true;
+            }
+        }
+        if(matchFound == false) {
+            cout << groupOne[i];
+            groupThree.push_back(groupOne[i]);
+        }
+        matchFound = false;
+    }
+
+    cout << "Answer: ";
 
 
     return 0;
